0a87e7e430dcd5393444e30a9dfe0c8c13823065
S-Achi-S <irosru@gmail.com>
2023-09-08 15:44:22 +0300

    update PERSON abstract class

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # EDU.SkillFactory.Unit6.Unit7.VW
 Итоговое задание 5 и 6 модулей
+
+
+
+### Program.cs
+
+
+### Person.cs
+Абстрактный класс | для всех пользователей
+
+
+#### PersonEmpoyee.cs
+> Сотрудник офиса
+
+> Сотрудник службы доставки
+
+
+#### PersonCustomer.cs
+
+
+##### PersonIdList.cs
+> Проверяет ID пользователей.
--- VWork/Person.cs
+++ VWork/Person.cs
@@ -1,69 +1,97 @@
 ﻿
 using System.Text;
 
-namespace VWork;
-
-
-public abstract class Person
+namespace VWork
 {
-    public string PersonName { set; get; }
-
-    protected internal byte PersonAge
+    
+    public abstract class Person
     {
-        get
+        public string PersonName { get; set; }
+
+        protected internal byte PersonAge
         {
-            return PersonAge;
-        }
-        set
-        {
-            if (value > 0)
+            get { return PersonAge; }
+            set
             {
-                PersonAge = value;
+                if (value > 0)
+                {
+                    PersonAge = value;
+                }
             }
         }
-    }
-    
-    protected internal string PersonPhone { get; set; }
 
-    public Person(string name, byte age, string phone)
-    {
-        PersonName = name;
-        PersonAge = age;
-        PersonPhone = phone;
-    }
-
-    public static string GenerateSymbolsArray()
-    {
-        Random random = new Random();
-        const string chars = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЫЭЮЯ0123456789";
-        StringBuilder str = new StringBuilder();
-
-        for (int i = 0; i < 14; i++)
-        {
-            str.Append(chars[random.Next(chars.Length)]);
-        }
-
-        string generatedString = str.ToString();
-        return generatedString;
-    }
-    
-    public static string GetPersonId(in bool boolPersonType)
-    {
-        bool isPersonType = boolPersonType;
-        string personId;
-
-        if (isPersonType)
-        {
-            
-        }
-        else
-        {
-            
-        }
-    }
-
-    public static void ProcessListFromIdNumbers()
-    {
+        protected internal string PersonPhone { get; set; }
         
+        protected internal string PersonEmail { get; set; }
+        
+        public string PersonCity { get; set; }
+
+        public static bool boolPersonType { get; set; }
+
+        private static HashSet<string> generatedIdNumbers = new HashSet<string>();
+
+        public Person(string name, byte age, string phone, bool personType)
+        {
+            PersonName = name;
+            PersonAge = age;
+            PersonPhone = phone;
+            boolPersonType = personType;
+        }
+
+        public static string GenerateSymbolsArray()
+        {
+            Random random = new Random();
+            const string chars = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЫЭЮЯ0123456789";
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < 14; i++)
+            {
+                str.Append(chars[random.Next(chars.Length)]);
+            }
+
+            string generatedString = str.ToString();
+            return generatedString;
+        }
+
+        public static string GetPersonId(in bool boolPersonType)
+        {
+            var isPersonType = boolPersonType;
+            var tempById = GenerateSymbolsArray();
+            string personId;
+
+            if (isPersonType) /// true, means EMPLOYEE
+            {
+                personId = "EMP-";
+                personId = personId + tempById;
+            }
+            else /// false, means CUSTOMER
+            {
+                personId = "CUS-";
+                personId = personId + tempById;
+            }
+
+            return personId;
+        }
+
+        public static string ProcessListFromIdNumbers()
+        {
+            var personId = GetPersonId(boolPersonType);
+
+            if (generatedIdNumbers.Contains(personId))
+            {
+                generatedIdNumbers.Add(personId);
+            }
+            else
+            {
+                Console.WriteLine($"{personId} - its not uniq");
+                GenerateSymbolsArray();
+                ProcessListFromIdNumbers();
+            }
+
+            return personId;
+        }
+
+        public abstract void Greeting();
     }
+
 }
